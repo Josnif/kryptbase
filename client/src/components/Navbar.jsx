a31e8a5a77046d593aee812d39eaf0034dd95671
joenif <josephnifemi89@gmail.com>
2022-03-10 16:56:19 +0100

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const Navbar = () => {
+    return (
+        <h1>Navbar</h1>
+    );
+}
+
+export default Navbar;
